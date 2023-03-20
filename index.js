093f2982dfd707239fbb090e10422786ec628070
AhmidouA <ahmedakkioui75@gmail.com>
2023-03-20 23:34:49 +0100

    Index Page

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+
+
+
 require("dotenv").config();
 const PORT = process.env.PORT ?? 3000;
 
+//EJS
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
+app.get('/', (req,res) => {
+    res.render("index")
+
+});
 
 
 app.listen(PORT, () => {
