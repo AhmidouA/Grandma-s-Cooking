26d3321b32a809d81b7582c720f944f5c24db834
AhmidouA <ahmedakkioui75@gmail.com>
2023-03-21 20:16:44 +0100

    feature /Post sinup page and Create User

--- routes/router.js
+++ routes/router.js
@@ -9,11 +9,24 @@ router.get('/', (req,res) => {
     res.render("index")
 });
 
-// signUp 
+
+
+// signUp Page
 router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
+router.post('/signup', (req, res) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.password
+    }
+    User.create(user).then((result)=>{
+        res.render("index")
+    }).catch((err) => {
+        console.error(err)
+    })
+})
 
 
 
