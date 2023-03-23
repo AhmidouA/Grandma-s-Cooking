54c96a5a9800ee0a74c3ad9a1e1f1844fbc014fb
AhmidouA <ahmedakkioui75@gmail.com>
2023-03-23 01:27:20 +0100

    feature signUp with passport Done

--- controller/userController.js
+++ controller/userController.js
@@ -2,8 +2,10 @@
 const User = require("../models/user");
 // la seul qui marche avec require  "chalk": "^4.1.2",
 const chalk = require("chalk");
-// hacher le mot de passe
-const bcrypt = require("bcrypt");
+// Module auth avec passport
+const passport = require('passport');
+// Module pour le lien entre moongose et passport pour les auth
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const userController = {
   // module Home Page
@@ -17,33 +19,23 @@ const userController = {
   },
 
   //module sinUp (form)
-  async signup(req, res) {
+  async signup(req, res, next) {
 
-    const hash = await bcrypt.hash(req.body.password, 10)
-
-    console.log(chalk.bgBlue("{ hash>>>>>>> }", hash));
-    const user = {
-      username: req.body.username,
-      password: hash,
-    };
-    console.log(chalk.bgBlue("{ user.username>>>>>>> }", user.username));
-
-    // Vérifier si le username existe déja dans la bdd
-    const existingUser = await User.findOne({username: user.username});
-    if (existingUser) {
-      console.log(chalk.red(`Le username est déjà utilisé: ${user.username}`));
-      res.send(`Le username est déjà utilisé: ${user.username}`)
-    }
+    const newUser = new User ({
+      username: req.body.username
+    });
 
     try {
-      await User.create(user)
-      console.log(chalk.bgBlue(`l'utilisateur ${user.username} a bien été inscrit `));
-      res.render("index")
+      //save le user (passport-local-mongoose)
+      await User.register(newUser, req.body.password);
+      console.log(chalk.bgBlue("{ newUser>>>>>>> }", newUser));
+      passport.authenticate("local")(req, res, function() {
+        res.render("index");
+      });
     } catch (err) {
       console.error(chalk.bgRedBright(err))
-      res.send(`l'utilisateur ${user.username} n'a pas pu etre inscrit`);
-
-    }
+      res.send(`l'utilisateur ${newUser.username} n'a pas pu etre inscrit`);
+    };
   },
 
   //module login Page
